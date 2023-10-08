7a97f8297c86dbf58bd330598c894a4153c235c3
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-10-08 21:36:33 +0700

    xong tạo doanh nghiệp

--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateBusinessDto } from './dto/create-business.dto';
 import { UpdateBusinessDto } from './dto/update-business.dto';
 import { Business } from './entities/business.entity';
@@ -8,6 +8,7 @@ import { TypeModelService } from 'src/type-model/type-model.service';
 import { TypeModel } from 'src/type-model/entities/type-model.entity';
 import { UsersService } from 'src/users/users.service';
 import { StorageService } from 'src/storage/storage.service';
+import { ImageTypes } from 'src/common/enum/file';
 
 @Injectable()
 export class BusinessService {
@@ -19,21 +20,32 @@ export class BusinessService {
 
   }
   async create(createBusinessDto: CreateBusinessDto): Promise<Business | any> {
-    const { typeModelId, userId } = createBusinessDto;
-    const [typeModel, user] = await Promise.all([
-      this.typeModelService.findOne(typeModelId),
-      this.userService.findOne(userId)
-    ]);
-    
-    const creating = this.businessRepository.create({
-      ...createBusinessDto,
-      user,
-      typeModel,
-      avatar: null,
-      licenseBusiness: null,
-      certificate: null,
-      inspectionPhoto: null,
-    })
+    try {
+      const { typeModelId, userId } = createBusinessDto;
+      const [typeModel] = await Promise.all([
+        this.typeModelService.findOne(typeModelId),
+        // this.userService.findOne(userId)
+      ]);
+      const avatar = await this.storageService.uploadMultiFiles(ImageTypes.CARD_BUSINESS_AVATAR, createBusinessDto.avatar)
+      const licenseBusiness = await this.storageService.uploadMultiFiles(ImageTypes.CARD_BUSINESS_LICENSE, createBusinessDto.licenseBusiness)
+      const certificate = await this.storageService.uploadMultiFiles(ImageTypes.CARD_BUSINESS_CERT, createBusinessDto.certificate)
+      const inspectionPhoto = await this.storageService.uploadMultiFiles(ImageTypes.CARD_BUSINESS_INSPECTION, createBusinessDto.inspectionPhoto)
+      const creating = this.businessRepository.create({
+        ...createBusinessDto,
+        user: null,
+        typeModel,
+        avatar,
+        licenseBusiness: licenseBusiness,
+        certificate: certificate,
+        inspectionPhoto: inspectionPhoto,
+      })
+      const saving = await this.businessRepository.save(creating);
+      return saving
+    } catch (error) {
+      throw new BadRequestException({
+        message: error.message,
+      })
+    }
   }
 
   findAll() {
--- src/common/enum/file.ts
+++ src/common/enum/file.ts
@@ -8,5 +8,9 @@ export enum ImageTypes {
     CARD_AREA = 'areas',
     CARD_MATERIAL = 'materials',
     CARD_USER_PROFILE_PHOTO_CERT = 'cert_user',
-    CARD_BUSINESS = 'business'
+    CARD_BUSINESS = 'business',
+    CARD_BUSINESS_AVATAR = 'business-avatar',
+    CARD_BUSINESS_LICENSE = 'business-license',
+    CARD_BUSINESS_CERT = 'business-certificate',
+    CARD_BUSINESS_INSPECTION = 'business-inspection',
 }
